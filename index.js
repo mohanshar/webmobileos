abd074a0f37e5ea6d5b8d0be448317a0ddf47dae
Mohan Sharma <mohansharma_48@live.com>
2023-06-10 08:17:07 +0545

    new update

--- index.js
+++ index.js
@@ -6,33 +6,34 @@
 $(document).ready(function () {
 
     $('.openlock').on("click", function () {
-        $('.openlock').slideUp();
-        $('#lock').slideDown();
+        $('.openlock').slideUp(1500);
+        $('.time').fadeOut(1000);
+        $('#lock').slideDown(1000);
     });
+    
 
-    $('.ok').on("click", function () {
-        $('.del').hide();
-    });
+    // $('.ok').on("click", function () {
+    //     $('.del').hide();
+    // });
 
 });
 
 
 $(document).on('click', '.ok', function () {
-    var answer = $("#textBox").val();
-    if (answer == 2513) {
-        $('.home').slideDown();
-        $('#lock').slideUp(function () {
-            // alert('Welcome to the brand new WebOS Mobile Operating System.');
-        });
-
+    var answer = $("#passcodeInput").val();
+    if (answer == 2222) {
+        $('.home').fadeIn(1500);
+        $('#lock').fadeOut(1500);
+        // $('#lock').slideUp(function () {
+        //     alert('Welcome to the brand new WebOS Mobile Operating System.');
+        // });
     }
     else if (answer.length === 0) {
-        alert('please put your passcode');
+        alert('please! put your passcode');
         window.navigator.vibrate(200);
     }
     else {
-        alert('wrong');
+        alert('wrong passcode, try again!');
         window.navigator.vibrate(200);
     }
 });
-
